48acc9eed990ee2b1ec39ef37d990bc2d25dd9e1
bryanobmasca <bryanobmasca@gmail.com>
2020-10-15 14:25:38 +0800

    [FizzBuzz][Bryan]refactor: extract constants WHIZZ and MODULO_7 and variable isModulo7

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -6,18 +6,21 @@ public class FizzBuzz {
     private static final String FIZZ = "Fizz";
     private static final int MODULO_5 = 5;
     private static final String BUZZ = "Buzz";
+    private static final int MODULO_7 = 7;
+    private static final String WHIZZ = "Whizz";
 
     public String countOff(int orderNumber) {
         boolean isModulo3 = orderNumber % MODULO_3 == 0;
         boolean isModulo5 = orderNumber % MODULO_5 == 0;
+        boolean isModulo7 = orderNumber % MODULO_7 == 0;
         if(isModulo3){
             return FIZZ;
         }
         if(isModulo5){
             return BUZZ;
         }
-        if(orderNumber % 7 == 0){
-            return "Whizz";
+        if(isModulo7){
+            return WHIZZ;
         }
         return String.valueOf(orderNumber);
     }
