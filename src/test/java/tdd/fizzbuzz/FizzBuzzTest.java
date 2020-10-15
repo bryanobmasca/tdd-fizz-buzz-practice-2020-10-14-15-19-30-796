3d692a7587ee80be1459b077ecb7f47999e3052b
bryanobmasca <bryanobmasca@gmail.com>
2020-10-15 14:31:32 +0800

    [FizzBuzz][Bryan]test: implement the multiple of 3 & 5 with input 15 and output FizzBuzz

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -56,4 +56,17 @@ public class FizzBuzzTest {
         //then
         assertEquals("Whizz", actual);
     }
+
+    @Test
+    void should_return_fizzbuzz_when_count_off_given_order_number_15() {
+        //given
+        int orderNumber = 15;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        //when
+        String actual = fizzBuzz.countOff(orderNumber);
+
+        //then
+        assertEquals("FizzBuzz", actual);
+    }
 }
