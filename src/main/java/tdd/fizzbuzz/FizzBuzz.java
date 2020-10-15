d3d4c7e3e99dae2cc51b0afeb66b0379b491af60
bryanobmasca <bryanobmasca@gmail.com>
2020-10-15 14:08:26 +0800

    [FizzBuzz][Bryan]feature: implement the multiple of 3 with output Fizz

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,7 +2,12 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
 
+    private static final String FIZZ = "Fizz";
+
     public String countOff(int orderNumber) {
+        if(orderNumber % 3 == 0){
+            return FIZZ;
+        }
         return String.valueOf(orderNumber);
     }
 }
