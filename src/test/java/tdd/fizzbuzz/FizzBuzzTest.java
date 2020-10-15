6e5ef2e448f1bff91041ef57d138f511eb49bb12
bryanobmasca <bryanobmasca@gmail.com>
2020-10-15 14:35:30 +0800

    [FizzBuzz][Bryan]test: implement the multiple of 3 & 7 with input 21 and output FizzWhizz

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -69,4 +69,17 @@ public class FizzBuzzTest {
         //then
         assertEquals("FizzBuzz", actual);
     }
+
+    @Test
+    void should_return_fizzwhizz_when_count_off_given_order_number_21() {
+        //given
+        int orderNumber = 21;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        //when
+        String actual = fizzBuzz.countOff(orderNumber);
+
+        //then
+        assertEquals("FizzWhizz", actual);
+    }
 }
